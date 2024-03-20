c4f82bf1586cf3b1fcc709730aa927b4dc29c6f8
Milton Chang <mchangpi@gmail.com>
2024-03-20 20:28:35 +0800

    Config and connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
-import { ApolloServer } from '@apollo/server';
+import dotenv from 'dotenv';
 
+import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 
 import mergedResolvers from './resolvers/index.js';
 import mergedTypeDefs from './typeDefs/index.js';
 
+import { connectDB } from './db/connectDB.js';
+
+dotenv.config();
 const app = express();
 const httpServer = http.createServer(app);
 
@@ -35,5 +39,6 @@ app.use(
 
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
 
 console.log(`🚀 Server ready at http://localhost:4000/`);
