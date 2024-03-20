ba295aed483045d5b7f9c322ddddfaff6e456c8c
Milton Chang <mchangpi@gmail.com>
2024-03-21 00:06:07 +0800

    Configure passport-local strategy https://github.com/passport/todos-express-password/blob/master/routes/auth.js https://www.npmjs.com/package/graphql-passport

---
+++ backend/passport/passport.config.js
@@ -0,0 +1,42 @@
+import passport from 'passport';
+import bcrypt from 'bcryptjs';
+
+import User from '../models/user.model.js';
+import { GraphQLLocalStrategy } from 'graphql-passport';
+
+export const configurePassport = async () => {
+  /* https://github.com/passport/todos-express-password/blob/master/routes/auth.js */
+  passport.serializeUser((user, done) => {
+    console.log('Serializing user');
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    console.log('Deserializing user');
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  /* https://www.npmjs.com/package/graphql-passport */
+  passport.use(
+    new GraphQLLocalStrategy(async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username });
+        if (!user) {
+          throw new Error('Invalid username or password');
+        }
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) {
+          throw new Error('Invalid username or password');
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    })
+  );
+};
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,15 +1,15 @@
 import { users } from '../dummyData/data.js';
 
 const userResolver = {
-	Query: {
-		users: () => {
-			return users;
-		},
-		user: (_, { userId }) => {
-			return users.find((user) => user._id === userId);
-		},
-	},
-	Mutation: {},
+  Query: {
+    users: (_, __, { req, res }) => {
+      return users;
+    },
+    user: (_, { userId }) => {
+      return users.find((user) => user._id === userId);
+    },
+  },
+  Mutation: {},
 };
 
 export default userResolver;
